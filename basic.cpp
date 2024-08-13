1dbdcffb493a0d855074efa900eff3acd1c15ca9
dangtrantrung <trung280584@gmail.com>
2024-08-13 17:43:35 +0700

    scope_global

--- basic.cpp
+++ basic.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main()
 {
+    int global_var=1000;
     vector<string> msg {"Hello Trung Dang dev with C++", "C++", "World", "from", "VS Code", "and the C++ extension!"};
     int i =99;
     std::string s = std::to_string(i);
@@ -21,7 +22,33 @@ int main()
     cout<< "size of double "<<sizeof(double)<<endl;
     cout<< "size of float "<<sizeof(float)<<endl;
     cout<< "size of long"<<sizeof(long)<<endl;
+    //scope
+    int main_var=0;
+    cout <<"scope"<<main_var<<endl;
+    { //scope 1
+        int scope_var_1=1;
+        cout <<"scope_1"<<scope_var_1<<endl;
+        {
+            cout <<"scope_1"<<scope_var_1<<endl;
 
+        }
+
+    }
+    { //scope 2
+        int scope_var_2=2;
+        int scope_var_1=4;
+        cout <<"scope_2"<<scope_var_2<<endl;
+        {
+            int scope_var_2=20;
+            int global_var=1001;
+
+            cout <<"scope_2"<<scope_var_2<<endl;
+            cout <<"scope_1"<<scope_var_1<<endl;
+            cout <<"scope_global"<<global_var<<endl;
+
+        }
+
+    }
 
 
 
