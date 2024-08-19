f4997bf6f76ad57bf4ed91a813cb7d13e73345d1
dangtrantrung <trung280584@gmail.com>
2024-08-20 02:04:20 +0700

    ex1 linear recursion

--- recursion/tinhgiaithua.cpp
+++ recursion/tinhgiaithua.cpp
@@ -8,9 +8,14 @@ int Giaithua(int n){
     }
     return n*(Giaithua(n-1)); //call self function
 }
+long Tong(int n){
+    if(n==0) return 0;
+    return(Tong(n-1)+n);
+}
 int main(){
     int n;
     cout<<"Nhap n:";
     cin>>n;
-    cout<<n<<"!="<<Giaithua(n);
+    cout<<n<<"!="<<Giaithua(n)<<endl;
+    cout<<"Tong 1+...+"<<n<<"="<<Tong(n)<<endl;
 }
