b38894f7f4f66a96a21cd9913655f4f026df1992
dangtrantrung <trung280584@gmail.com>
2024-08-22 18:37:46 +0700

    pointer

--- Basic/Pointer.cpp
+++ Basic/Pointer.cpp
@@ -5,10 +5,10 @@ int main() {
     // Write C++ code here
     std::cout << "Try programiz.pro";
     //Pointer, Container: List, Stack, Queue,Set, Map
-    
+
     //1.Pointer
     int *pointerInt; //declare
-    int* pointerInt1; 
+    int* pointerInt1;
     int x=5, *px;
     px=&x; //assign pointer address of x, not value of x;
     *px=x; //assign value of x to px;
@@ -19,8 +19,8 @@ int main() {
      cout<<px2<<endl<<px1<<endl;
     //*px1=x; //assign value of x to px1;
     px2=px1; //assgin address of px1 to px2, and value *px2
-    
+
     cout<<endl<<&x<<endl<<x<<endl<<px<<endl<<y<<endl<<*px<<endl<<&y<<endl<<px1<<*px1<<endl<<y1<<endl<<&y<<endl<<*px2<<endl<<*px1<<endl<<px2<<endl<<px1<<endl;
-    
+
     return 0;
 }
