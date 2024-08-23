79be3b0885c0d02f4e6c27c9185a0d65ea7e5c97
dangtrantrung <trung280584@gmail.com>
2024-08-24 06:41:58 +0700

    *p++, --

--- Pointer/Pointer-manipulation.cpp
+++ Pointer/Pointer-manipulation.cpp
@@ -8,8 +8,12 @@ int main(){
     cout<<a<<endl<<*p1<<endl<<*p2;
     *p1=5;
     cout<<"thay doi gia tri con tro"<<endl;
-    cout<<a<<endl<<*p1<<endl<<*p2;
+    cout<<a<<endl<<*p1<<endl<<*p2<<endl<<p1<<endl<<p2<<endl;
     // *p++ *p--: change address of pointer by data type - number of byte
+    *p1++;
+    *p2++;
+    cout<<p1<<endl<<p2<<endl;
+
     //permutation
     int x=10,y=20,*px,*py, temp;
     px=&x;
