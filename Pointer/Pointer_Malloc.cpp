17cb50239cd0f9c51ce1be06a7e41ceac7820dc2
dangtrantrung <trung280584@gmail.com>
2024-08-24 13:26:18 +0700

    void pointer

--- Pointer/Pointer_Malloc.cpp
+++ Pointer/Pointer_Malloc.cpp
@@ -21,9 +21,17 @@ int main(){
    }
    else{
    cout<<p1<<endl;
-    cout<<*p1<<endl;
+   cout<<*p1<<endl;
    }
-
+   free(p);
+   free(p1);
+   cout<<"sau khi free ..."<<endl;
+   cout<<p<<endl;
+   cout<<p1<<endl;
+//    p=NULL;
+//    p1=NULL;
+   cout<<*p<<endl;
+   cout<<*p1<<endl;
    // dynamic memory allocation-heap by C++
    int *p2=new int;
    if(p2==NULL){
@@ -38,4 +46,19 @@ int main(){
     delete p2;
     p2=NULL;
    }
+   //void pointer
+   int a=5;
+   float f=1.5;
+   int* pa;
+   float* pf;
+   void *pvoid;
+   pa=&a;
+   pf=&f;
+   pvoid=&a;
+   cout<<" int pointer void="<<*((int*)pvoid);
+   pvoid=&f;
+   cout<<"\n float pointer void="<<*((float*)pvoid)<<endl;
+
+
+
 }
