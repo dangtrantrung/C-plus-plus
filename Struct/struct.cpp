ddf52c58fb2f09973de7e8336e4d1aae22b73d7b
dangtrantrung <trung280584@gmail.com>
2024-08-26 21:52:16 +0700

    array & pointer of struct

--- Struct/struct.cpp
+++ Struct/struct.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
-struct NgayThangNamSinh{
+typedef struct NgayThangNamSinh{
     int NgaySinh;
     int ThangSinh;
     int NamSinh;
 };
-struct SinhVien{
+typedef struct SinhVien{
     char MSSV[10];
     float Diem;
     // int NgaySinh;
@@ -14,16 +14,31 @@ struct SinhVien{
     NgayThangNamSinh sinhNgay;
 };
 
+typedef struct DANHSACH{
+char hoten[25];
+float toan,ly,hoa;
+};
+typedef struct Diem{
+int x;
+int y;
+};
 
 int main(){
+DANHSACH bangdiem[50];
 SinhVien sv1,sv2;
 sv1.Diem=9.0;
 sv1.sinhNgay.NgaySinh=1;
 sv1.sinhNgay.ThangSinh=10;
 sv1.sinhNgay.NamSinh=2020;
 sv1.sinhNgay.NamSinh=sv2.sinhNgay.NamSinh;
-    cout<<sv1.sinhNgay.NamSinh<<endl<<sv2.sinhNgay.NamSinh<<endl;
-    cout<<sv1.Diem<<endl;
+cout<<sv1.sinhNgay.NamSinh<<endl<<sv2.sinhNgay.NamSinh<<endl;
+cout<<sv1.Diem<<endl;
+Diem *p,a;
+p=&a;
+p->x=10;
+p->y=100;
+cout<<a.x<<endl;
+cout<<a.y;
 
-    return 0;
+return 0;
 }
