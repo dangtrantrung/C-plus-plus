66ec5010896e1ba1a983635bbb220c5a17808c22
dangtrantrung <trung280584@gmail.com>
2024-08-26 21:31:41 +0700

    struct 2

--- Struct/struct.cpp
+++ Struct/struct.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
-
+struct NgayThangNamSinh{
+    int NgaySinh;
+    int ThangSinh;
+    int NamSinh;
+};
 struct SinhVien{
     char MSSV[10];
     float Diem;
@@ -8,9 +12,18 @@ struct SinhVien{
     // int ThangSinh;
     // int NamSinh;
     NgayThangNamSinh sinhNgay;
-}
-typedef struct NgayThangNamSinh{
-    int NgaySinh;
-    int ThangSinh;
-    int NamSinh;
+};
+
+
+int main(){
+SinhVien sv1,sv2;
+sv1.Diem=9.0;
+sv1.sinhNgay.NgaySinh=1;
+sv1.sinhNgay.ThangSinh=10;
+sv1.sinhNgay.NamSinh=2020;
+sv1.sinhNgay.NamSinh=sv2.sinhNgay.NamSinh;
+    cout<<sv1.sinhNgay.NamSinh<<endl<<sv2.sinhNgay.NamSinh<<endl;
+    cout<<sv1.Diem<<endl;
+
+    return 0;
 }
