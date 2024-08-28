d3be6e5baa844a6c41d9d60981f64a3d6f857838
dangtrantrung <trung280584@gmail.com>
2024-08-29 05:40:39 +0700

    memory allocation

--- OOP/OOP1.cpp
+++ OOP/OOP1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <malloc.h>
 using namespace std;
 int x=10;
 int main(){
@@ -6,5 +7,16 @@ int main(){
     int x=5;
     cout<< "x trong main"<<x<<endl;
     cout<< "x ngoai main"<<::x<<endl; //access operator
+    //malloc C
+     int* p;
+     p=(int*)malloc(sizeof(int));
+     free(p);
+     // C++
+     int* ptn =new int;
+     cout<<ptn;
+     delete ptn;
+     cout<<ptn;
+
+
     return 0;
 }
