b0ff4beb4ccf84bb71615224fe28c558913bc13b
dangtrantrung <trung280584@gmail.com>
2024-08-29 05:51:22 +0700

    swap

--- OOP/OOP1.cpp
+++ OOP/OOP1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <malloc.h>
+#include <time.h>
 using namespace std;
 int x=10;
 int main(){
@@ -16,6 +17,27 @@ int main(){
      cout<<ptn;
      delete ptn;
      cout<<ptn;
+     //array pointer
+    int n;
+    cout<<"nhap n";
+    cin>>n;
+    int* p1;
+    p1= new int[n];
+   for(int i=0;i<n;i++){
+    cout<<"nhap phan tu thu"<<i<<endl;
+    cin>>*(p+i);
+   }
+   for(int i=0;i<n-1;i++){
+    for (int j=i+1;j<n;j++){
+        if(*(p+i)>*(p+j)){
+            swap(*(p+i),*(p+j));
+        }
+    }
+   }
+   for(int i=0;i<n;i++){
+
+    cout<<*(p+i)<<"\t";
+   }
 
 
     return 0;
