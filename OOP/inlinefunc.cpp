32d0ce0b6d506f5aee096c5a2d30d25ed83697b0
dangtrantrung <trung280584@gmail.com>
2024-08-30 06:52:22 +0700

    inline functions

---
+++ OOP/inlinefunc.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+inline int tinhdientich(int cd, int cr){
+    return cd*cr;
+}
+int main(){
+int d=60;
+int r=10;
+  cout<<"Dien tich HCN la: "<<tinhdientich(d,r)<<endl;
+    return 0;
+}
