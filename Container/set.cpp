47770074d5ba32381e33db4b8165d9a6071505f1
dangtrantrung <trung280584@gmail.com>
2024-09-07 13:13:00 +0700

    class

--- Container/set.cpp
+++ Container/set.cpp
@@ -9,36 +9,63 @@ using namespace std;
 //erase
 
 int main(){
-    set<int> s;
-    s.insert(100);
-    s.insert(200);
-    s.insert(300);
-    s.insert(100);
-    cout<<s.size()<<endl;
-    set<int> s1;
-    for (int i=0;i<10;i++)
-    {
-        s1.insert(i);
-    }
-    if(s1.count(5)) cout<<"Found 5 in set"<<endl;
-    else cout<<"Not found 5";
-    s1.erase(9);
-    cout<<s1.size()<<endl;
-    if(s1.find(9)!=s1.end()) cout<<"Found 9 in set"<<endl;
-    else cout<<"Not found 9"<<endl;
-    //erase by iterator
-    s1.erase(s1.find(6));
-    for(int x:s1) cout<<x<<",";
-    cout<<endl;
-    cout<<*s1.begin()<<endl;
-    cout<<*s1.rbegin()<<endl;
-    cout<<*s1.end()<<endl;
-    for(auto x:s1) cout<<x<<"-";
-    cout<<endl;
-    for(set<int>::iterator it=s1.begin();it!=s1.end();++it )
-    {
-        cout<<*it<<" ";
-    }
+//     set<int> s;
+//     s.insert(100);
+//     s.insert(200);
+//     s.insert(300);
+//     s.insert(100);
+//     cout<<s.size()<<endl;
+//     set<int> s1;
+//     for (int i=0;i<10;i++)
+//     {
+//         s1.insert(i);
+//     }
+//     if(s1.count(5)) cout<<"Found 5 in set"<<endl;
+//     else cout<<"Not found 5";
+//     s1.erase(9);
+//     cout<<s1.size()<<endl;
+//     if(s1.find(9)!=s1.end()) cout<<"Found 9 in set"<<endl;
+//     else cout<<"Not found 9"<<endl;
+//     //erase by iterator
+//     s1.erase(s1.find(6));
+//     for(int x:s1) cout<<x<<",";
+//     cout<<endl;
+//     cout<<*s1.begin()<<endl;
+//     cout<<*s1.rbegin()<<endl;
+//     cout<<*s1.end()<<endl;
+//     for(auto x:s1) cout<<x<<"-";
+//     cout<<endl;
+//     for(set<int>::iterator it=s1.begin();it!=s1.end();++it )
+//     {
+//         cout<<*it<<" ";
+//     }
+//   //EX
+//     set<int> s2;
+//     int n;cin>>n;
+//     for (int i=0;i<n;i++){
+//         int x;cin>>x;s2.insert(x);
+//     }
+//     cout<<"count unique elements in array set="<<s2.size()<<endl;
+//     //multiset
+//     multiset<int> ms;
+//     ms.insert(100);
+//     ms.insert(100);
+//     ms.insert(200);
+//     ms.insert(200);
+//     cout<<ms.size()<<endl;
+//     for(auto x:ms) cout<<x<<" ";
+//     cout<<ms.count(100)<<endl;
+ 
+
+
+
+
+
+
+
+
+
+
 
     return 0;
 }
---
+++ OOP/OOP1/Class.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Character{
+    //methods:
+    public:
+    void takeDamage(int damage){
+
+    }
+    void attack(Character &target){
+
+    }
+    void takeHP(int healthpoint){
+
+    }
+    void changeWeapon(string newWeapon,int dmgNewWeapon){
+
+    }
+    bool isAlive(){
+
+    }
+    //properties:
+    private:
+    int m_hp;
+    int m_mp;
+    string m_weapon;
+    int m_dmgWeaponl;
+};
+
+int main(){
+    Character david, goliah;
+    goliah.attack(david);
+    david.takeHP(20);
+    goliah.attack(david);
+    david.attack(goliah);
+    goliah.changeWeapon("The Deaths\'s battle-axe",40);
+    goliah.attack(david);
+    return 0;
+}
