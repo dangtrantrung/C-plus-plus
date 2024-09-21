d9c56fc6d085fb160f6f51e03a1eadd960cac108
dangtrantrung <trung280584@gmail.com>
2024-09-21 13:55:56 +0700

    QWidget 1

--- Qt framework/t2/main.cpp
+++ Qt framework/t2/main.cpp
@@ -3,12 +3,25 @@
 #include <QPushButton>
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
+//     QApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-    QPushButton btn("Hello Fresher C++!");
-    //
-    btn.show();
-    return app.exec();
+//     QQmlApplicationEngine engine;
+//     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+//     QPushButton btn("Hello Fresher C++!");
+//     //
+//     btn.show();
+//     return app.exec();
+        QApplication app(argc, argv);
+        //create a new window
+        QWidget window;
+        window.setFixedSize(300, 150);
+
+        //create a button in window
+        QPushButton btn("Alert !", &window);
+        btn.setFont(QFont("Comic Sans MS", 14));
+        btn.setCursor(Qt::PointingHandCursor);
+
+        window.show();
+
+        return app.exec();
 }
