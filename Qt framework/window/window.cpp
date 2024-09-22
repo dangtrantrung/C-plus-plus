0c173bb81e5fd55a3ae2eae92fbfd77cafbf3bb0
dangtrantrung <trung280584@gmail.com>
2024-09-22 08:42:29 +0700

    connect SIGNAL click btn with SLOT quit()

--- Qt framework/window/window.cpp
+++ Qt framework/window/window.cpp
@@ -15,10 +15,12 @@ window::window(int h, int w):QWidget()
   m_btn=new QPushButton("Alert set h,w for window!",this);
   m_btn->setFont(QFont("Comic Sans MS",14));
   m_btn->setCursor(Qt::PointingHandCursor);
+  //connect SIGNAL click btn with SLOT quit()
+  QObject::connect(m_btn,SIGNAL(clicked()),qApp,SLOT(quit()));
+
 }
 
 window::~window()
 {
   delete m_btn;
 }
-
