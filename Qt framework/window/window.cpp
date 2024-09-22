17b41e8d4a1a0d5435ae7aedb39e7dfbdecb47ac
dangtrantrung <trung280584@gmail.com>
2024-09-22 08:58:42 +0700

    SIGNAL change slider with SLOT display of lcd

--- Qt framework/window/window.cpp
+++ Qt framework/window/window.cpp
@@ -17,7 +17,14 @@ window::window(int h, int w):QWidget()
   m_btn->setCursor(Qt::PointingHandCursor);
   //connect SIGNAL click btn with SLOT quit()
   QObject::connect(m_btn,SIGNAL(clicked()),qApp,SLOT(quit()));
-
+  //set lcd number and slider
+  m_lcd=new QLCDNumber(this);
+  m_lcd->setSegmentStyle(QLCDNumber::Flat);
+  m_lcd->move(110,20);
+  m_slider= new QSlider(Qt::Horizontal,this);
+  m_slider->setGeometry(10,60,150,20);
+  //connect SIGNAL value change slider with SLOT display of lcd
+  QObject::connect(m_slider,SIGNAL(valueChanged(int)),m_lcd,SLOT(display(int)));
 }
 
 window::~window()
