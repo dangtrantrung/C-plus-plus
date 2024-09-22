7bf2e259802d538eae9f5aa79eb8376317e2a688
dangtrantrung <trung280584@gmail.com>
2024-09-22 12:43:07 +0700

    Qt public slots: & signals:

--- Qt framework/window/window.cpp
+++ Qt framework/window/window.cpp
@@ -7,6 +7,26 @@ window::window():QWidget()
   m_btn=new QPushButton("Alert!",this);
   m_btn->setFont(QFont("Comic Sans MS",14));
   m_btn->setCursor(Qt::PointingHandCursor);
+  //connect SIGNAL click btn with SLOT quit()
+  QObject::connect(m_btn,SIGNAL(clicked()),qApp,SLOT(quit()));
+  //set lcd number and slider
+  m_lcd=new QLCDNumber(this);
+  m_lcd->setSegmentStyle(QLCDNumber::Flat);
+  m_lcd->move(50,38);
+  m_slider= new QSlider(Qt::Horizontal,this);
+  m_slider->setRange(100, 600);
+  m_slider->setGeometry(10,60,150,20);
+  //connect SIGNAL value change slider with SLOT display of lcd
+  QObject::connect(m_slider,SIGNAL(valueChanged(int)),m_lcd,SLOT(display(int)));
+  //QProgressBar
+  m_progressBar=new QProgressBar(this);
+  m_progressBar->move(30,80);
+  // m_progressBar->setGeometry(20,50,10,20);
+  QObject::connect(m_slider,SIGNAL(valueChanged(int)),m_progressBar,SLOT(setValue(int)));
+  //connect m_slider to this window new public SLOT
+  QObject::connect(m_slider,SIGNAL(valueChanged(int)),this,SLOT(changeWidthofWindow(int)));
+  //connect maxSize signal
+  QObject::connect(this,SIGNAL(maxSize()),qApp,SLOT(quit()));
 }
 window::window(int h, int w):QWidget()
 {
@@ -25,17 +45,30 @@ window::window(int h, int w):QWidget()
   m_slider->setGeometry(10,60,150,20);
   //connect SIGNAL value change slider with SLOT display of lcd
   QObject::connect(m_slider,SIGNAL(valueChanged(int)),m_lcd,SLOT(display(int)));
-   //QProgressBar
+  //QProgressBar
   m_progressBar=new QProgressBar(this);
   m_progressBar->move(300,150);
   // m_progressBar->setGeometry(20,50,10,20);
   QObject::connect(m_slider,SIGNAL(valueChanged(int)),m_progressBar,SLOT(setValue(int)));
+  //connect m_slider to this window new public SLOT
+  QObject::connect(m_slider,SIGNAL(valueChanged(int)),this,SLOT(changeWidthofWindow(int)));
+  //connect maxSize signal
+  QObject::connect(this,SIGNAL(maxSize()),qApp,SLOT(quit()));
+
 }
+void window::changeWidthofWindow(int w){
+setFixedSize(w,150);
+if(w==600){
+    emit maxSize();
+}
+}
+
 
 window::~window()
 {
-  delete m_btn;
-  delete m_progressBar;
-  delete m_slider;
-  delete m_lcd;
+      delete m_btn;
+      delete m_progressBar;
+      delete m_slider;
+      delete m_lcd;
 }
+
--- Qt framework/window/window.h
+++ Qt framework/window/window.h
@@ -4,15 +4,27 @@
 #include <QApplication>
 #include <QWidget>
 #include <QPushButton>
+#include <QLCDNumber>
+#include <QSlider>
+#include <QProgressBar>
 
 class window:public QWidget
 {
+    Q_OBJECT
 public:
     window();
     window(int h, int w);
     ~window();
 private:
     QPushButton *m_btn;
+    QLCDNumber *m_lcd;
+    QSlider *m_slider;
+    QProgressBar *m_progressBar;
+public slots:
+    void changeWidthofWindow(int w);
+signals:
+    void maxSize();
+
 };
 
 #endif // WINDOW_H
