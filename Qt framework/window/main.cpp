aa538244f59d9503dc1cb7755cb951d4d9d511b2
dangtrantrung <trung280584@gmail.com>
2024-09-28 11:44:37 +0700

    QGridLayout

--- Qt framework/window/main.cpp
+++ Qt framework/window/main.cpp
@@ -15,6 +15,22 @@ int main(int argc, char *argv[])
     w.show();
     window w1(500,400);
     w1.show();
+    QWidget w3;
+    QPushButton *b4=new QPushButton("Hello");
+    QPushButton *b5=new QPushButton("New");
+    QPushButton *b6=new QPushButton("Fresher C++");
+    QPushButton *b7=new QPushButton("__++_++__");
+
+    QGridLayout *layout1=new QGridLayout();
+    layout1->addWidget(b4,0,0);
+    layout1->addWidget(b5,0,1);
+    layout1->addWidget(b6,1,0);
+    layout1->addWidget(b7,1,1);
+
+    w3.setLayouts(layout1);
+    w3.show();
+
+
 
     return app.exec();
 }
